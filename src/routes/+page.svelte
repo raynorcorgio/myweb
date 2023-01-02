346e06bc97fd609b581b94b2e6dc0cd721a791d1
Raynor Corgio <raynor.corgio@student.saultcollege.ca>
2023-01-01 19:07:35 -0500

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,1 @@
+<h1 class="text-3xl font-bold underline text-purple-600">Hello sveltekit with github pages!</h1>
---
+++ svelte.config.js
@@ -0,0 +1,16 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+const dev = process.argv.includes('dev');
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter(),
+		paths: {
+			base: dev ? '' : '/myweb'
+		}
+	},
+	preprocess: vitePreprocess()
+};
+
+export default config;
