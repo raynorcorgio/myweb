84e10dd761ff9a5027bc7698f22fab1b0399b247
Raynor Corgio <raynor.corgio@student.saultcollege.ca>
2023-01-01 19:37:10 -0500

    Updates

---
+++ _app/immutable/chunks/1-d591ad16.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-1f1b903e.js";export{t as component};
---
+++ _app/immutable/chunks/3-8675f752.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/about/_page.svelte-4361ed58.js";export{t as component};
